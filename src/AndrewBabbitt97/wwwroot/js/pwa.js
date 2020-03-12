54f10491cd65942336b63bb10ebf695d4a5ab9fb
Andrew Babbitt <andrew.babbitt@outlook.com>
2020-03-12 17:06:07 -0600

    Updated PWA files to use the ones from Blazor

--- src/AndrewBabbitt97/wwwroot/js/pwa.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/sw.js').then(function(reg) {
-        console.log('Successfully registered service worker');
-    }).catch(function (err) {
-        console.log('Error registering service worker');
-    })
-}
